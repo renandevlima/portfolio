6744cab8ea737a89ab505d3b969794874ff60e04
Renan Lima <renanlima@Mac-mini-de-Renan.local>
2021-12-27 15:08:13 -0300

    Atualizado Curriculo

--- src/components/conhecimentos/Conhecimentos.js
+++ src/components/conhecimentos/Conhecimentos.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Conhecimentos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
-import { faHtml5, faCss3Alt, faJs, faReact, faVuejs, faAngular, faPhp, faLaravel, faWordpress, faMagento, faJava, faPython, faGitAlt } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase, faTools } from '@fortawesome/free-solid-svg-icons';
+import { faHtml5, faCss3Alt, faJs, faReact, faVuejs, faAngular, faPhp, faLaravel, faWordpress, faMagento, faJava, faPython, faGitAlt, faDocker} from '@fortawesome/free-brands-svg-icons';
 
 function Conhecimentos() {
+    const [showFront, setShowFront] = useState(false);
+    const [showBack, setShowBack] = useState(false);
+    const [showData, setShowData] = useState(false);
+    const [showTool, setShowTool] = useState(false);
+
     return (
         <section className='conhecimentos'>
             <div className='conhecimentos-conteudo' id='conhecimentos'>
                 <h2>Meus Conhecimentos</h2>
+                <div className='box-conhecimento'>
+                    <button className='show-conhecimento' onClick={() => setShowFront(!showFront)}><FontAwesomeIcon icon={faHtml5} className='icone'/>FrontEnd</button>
+                    <button className='show-conhecimento' onClick={() => setShowBack(!showBack)}><FontAwesomeIcon icon={faPhp} className='icone'/>BackEnd</button>
+                    <button className='show-conhecimento' onClick={() => setShowData(!showData)}><FontAwesomeIcon icon={faDatabase} className='icone'/> Banco de Dados</button>
+                    <button className='show-conhecimento' onClick={() => setShowTool(!showTool)}><FontAwesomeIcon icon={faTools} className='icone'/> Ferramentas</button>
+                </div>
                 <ul className='lista-conhecimentos'>
+                    { showFront && <React.Fragment>
                     <li className='conhecimento'><FontAwesomeIcon icon={faHtml5} className='icone'/> HTML</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faCss3Alt} className='icone'/> CSS</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faCss3Alt} className='icone'/> Bootstrap</li>
@@ -19,10 +31,14 @@ function Conhecimentos() {
                     <li className='conhecimento'><FontAwesomeIcon icon={faJs} className='icone'/> Jquery</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faReact} className='icone'/> React</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faReact} className='icone'/> Redux</li>
+                    <li className='conhecimento'><FontAwesomeIcon icon={faReact} className='icone'/> Redux Tools</li>
+                    <li className='conhecimento'><FontAwesomeIcon icon={faReact} className='icone'/> Redux Saga</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faReact} className='icone'/> React Native</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faVuejs} className='icone'/> Vue</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faVuejs} className='icone'/> Vuex</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faAngular} className='icone'/> Angular</li>
+                    </React.Fragment> }
+                    { showBack && <React.Fragment>
                     <li className='conhecimento'><FontAwesomeIcon icon={faPhp} className='icone'/> PHP</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faPhp} className='icone'/> CodeIgniter</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faLaravel} className='icone'/> Laravel</li>
@@ -30,10 +46,19 @@ function Conhecimentos() {
                     <li className='conhecimento'><FontAwesomeIcon icon={faMagento} className='icone'/> Magento</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faJava} className='icone'/> Java</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faPython} className='icone'/> Python</li>
+                    </React.Fragment> }
+                    { showData && <React.Fragment>
                     <li className='conhecimento'><FontAwesomeIcon icon={faDatabase} className='icone'/> MySQL</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faDatabase} className='icone'/> PostgreSQL</li>
                     <li className='conhecimento'><FontAwesomeIcon icon={faDatabase} className='icone'/> Mongo DB</li>
+                    </React.Fragment> }
+                    { showTool && <React.Fragment>
                     <li className='conhecimento'><FontAwesomeIcon icon={faGitAlt} className='icone'/> Git</li>
+                    <li className='conhecimento'><FontAwesomeIcon icon={faGitAlt} className='icone'/> GitHub</li>
+                    <li className='conhecimento'><FontAwesomeIcon icon={faGitAlt} className='icone'/> BitBucket</li>
+                    <li className='conhecimento'><FontAwesomeIcon icon={faGitAlt} className='icone'/> Firebase</li>
+                    <li className='conhecimento'><FontAwesomeIcon icon={faDocker} className='icone'/> Docker</li>
+                    </React.Fragment> }
                 </ul>
             </div>
         </section>
--- src/components/experiencia/Experiencia.js
+++ src/components/experiencia/Experiencia.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Experiencia.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock, faBriefcase, faCog } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faBriefcase, faCog, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 import fcl from '../../img/fcl.png';
 import bravo from '../../img/bravo.png';
 import prestus from '../../img/prestus-logo.png';
 import zarpo from '../../img/zarpo.png';
 import proa from '../../img/proa.svg';
+import adias from '../../img/adias.png';
 
 function Experiencia() {
+    const [hidden, setHidden] = useState(true);
+
     return (
         <section className='experiencia'>
             <div className='experiencia-conteudo' id='experiencia'>
                 <h2>Experiências Profissionais</h2>
+                <div className='expe'>
+                    <a className='expe-img proa' target='_blank' rel='noreferrer' href='https://www.adias.com.br/'>
+                        <img src={adias} alt='Adias Ar Condicionado' />
+                    </a>
+                    <div className='expe-texto'>
+                        <h4>Adias Ar Condicionado</h4>
+                        <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Programador FrontEnd Sr.</p>
+                        <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Outubro de 2021 até atualmente</p>
+                        <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Desenvolver aplicações em react fazendo desde a parte de diagramação, consumo de APIs, controle de rotas e gerenciamento de estados.</p>
+                    </div>
+                </div>
                 <div className='expe'>
                     <a className='expe-img proa' target='_blank' rel='noreferrer' href='https://www.proa.org.br/'>
                         <img src={proa} alt='Instituto PROA' />
@@ -21,54 +35,61 @@ function Experiencia() {
                     <div className='expe-texto'>
                         <h4>Instituto PROA</h4>
                         <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Líder Técnico de Desenvolvimento de Software</p>
-                        <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Junho de 2021 até Atualmente</p>
+                        <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Junho de 2021 até Outubro de 2021</p>
                         <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Executar manutenção no sistema de ensino a distancia em Moodle, desenvolvendo novas tecnologias para o instituto e coordenando atividades relacionadas a Desenvolvimento.</p>
                     </div>
                 </div>
-                <div className='expe'>
-                    <a className='expe-img' target='_blank' rel='noreferrer' href='https://fcl.com.br/'>
-                        <img src={fcl} alt='Fundação Cásper Líbero' />
-                    </a>
-                    <div className='expe-texto'>
-                        <h4>Fundação Cásper Líbero</h4>
-                        <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Programdor de Sistemas SR.</p>
-                        <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Maio de 2020 até Junho de 2021</p>
-                        <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Criação e manutenção de temas e plugins de Wordpress. Verificar Google Search console e elaborar soluções para erros encontrados nos sites da Fundação. Elaboração de estratégias de otimização de desempenho de sites.</p>
+                <div hidden={hidden}>
+                    <div className='expe'>
+                        <a className='expe-img' target='_blank' rel='noreferrer' href='https://fcl.com.br/'>
+                            <img src={fcl} alt='Fundação Cásper Líbero' />
+                        </a>
+                        <div className='expe-texto'>
+                            <h4>Fundação Cásper Líbero</h4>
+                            <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Programdor de Sistemas SR.</p>
+                            <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Maio de 2020 até Junho de 2021</p>
+                            <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Criação e manutenção de temas e plugins de Wordpress. Verificar Google Search console e elaborar soluções para erros encontrados nos sites da Fundação. Elaboração de estratégias de otimização de desempenho de sites.</p>
+                        </div>
+                    </div>
+                    <div className='expe'>
+                        <a className='expe-img' target='_blank' rel='noreferrer' href='https://www.bravocorp.com.br/'>
+                            <img src={bravo} alt='Bravo - Projects and OutSourcing' />
+                        </a>
+                        <div className='expe-texto'>
+                            <h4>Bravo - Projects and OutSourcing</h4>
+                            <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Programdor de Sistemas JR.</p>
+                            <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Fevereiro de 2020 até Maio de 2020</p>
+                            <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Criação de sistemas fiscais para clientes externos. Integração de Sistemas utilizando frontend e APIs em Laravel.</p>
+                        </div>
+                    </div>
+                    <div className='expe'>
+                        <a className='expe-img' target='_blank' rel='noreferrer' href='https://www.prestus.com.br/'>
+                            <img src={prestus} alt='Prestus - Secretárias Compartilhadas' />
+                        </a>
+                        <div className='expe-texto'>
+                            <h4>Prestus - Secretárias Compartilhadas</h4>
+                            <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Programdor PHP</p>
+                            <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Setembro de 2019 até Fevereiro de 2020</p>
+                            <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Criação e manutenção de sistemas internos e externos em PHP. Criação e integração de APIs utilizando framework próprio em PHP. Criação do WebSite da empresa utilizando tema prórpio em Wordpress.</p>
+                        </div>
+                    </div>
+                    <div className='expe'>
+                        <a className='expe-img' target='_blank' rel='noreferrer' href='https://www.zarpo.com.br/'>
+                            <img src={zarpo} alt='Zarpo Viagens' />
+                        </a>
+                        <div className='expe-texto'>
+                            <h4>Zarpo Viagens</h4>
+                            <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Estagiário</p>
+                            <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Janeiro de 2019 até Setembro de 2019</p>
+                            <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Manutenção de computadores da Empresa. Criação de soluções de suporte e Help Desk. Manutenção no frontend do website da empresa.</p>
+                        </div>
                     </div>
                 </div>
-                <div className='expe'>
-                    <a className='expe-img' target='_blank' rel='noreferrer' href='https://www.bravocorp.com.br/'>
-                        <img src={bravo} alt='Bravo - Projects and OutSourcing' />
-                    </a>
-                    <div className='expe-texto'>
-                        <h4>Bravo - Projects and OutSourcing</h4>
-                        <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Programdor de Sistemas JR.</p>
-                        <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Fevereiro de 2020 até Maio de 2020</p>
-                        <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Criação de sistemas fiscais para clientes externos. Integração de Sistemas utilizando frontend e APIs em Laravel.</p>
-                    </div>
-                </div>
-                <div className='expe'>
-                    <a className='expe-img' target='_blank' rel='noreferrer' href='https://www.prestus.com.br/'>
-                        <img src={prestus} alt='Prestus - Secretárias Compartilhadas' />
-                    </a>
-                    <div className='expe-texto'>
-                        <h4>Prestus - Secretárias Compartilhadas</h4>
-                        <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Programdor PHP</p>
-                        <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Setembro de 2019 até Fevereiro de 2020</p>
-                        <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Criação e manutenção de sistemas internos e externos em PHP. Criação e integração de APIs utilizando framework próprio em PHP. Criação do WebSite da empresa utilizando tema prórpio em Wordpress.</p>
-                    </div>
-                </div>
-                <div className='expe'>
-                    <a className='expe-img' target='_blank' rel='noreferrer' href='https://www.zarpo.com.br/'>
-                        <img src={zarpo} alt='Zarpo Viagens' />
-                    </a>
-                    <div className='expe-texto'>
-                        <h4>Zarpo Viagens</h4>
-                        <p><strong><FontAwesomeIcon icon={faBriefcase} /> Cargo: </strong>Estagiário</p>
-                        <p><strong><FontAwesomeIcon icon={faClock} /> Tempo de atuação: </strong>De Janeiro de 2019 até Setembro de 2019</p>
-                        <p><strong><FontAwesomeIcon icon={faCog} /> Funções exercidas: </strong>Manutenção de computadores da Empresa. Criação de soluções de suporte e Help Desk. Manutenção no frontend do website da empresa.</p>
-                    </div>
-                </div>
+                <button className='show-more' onClick={() => setHidden(!hidden)}>
+                    {hidden ?  
+                    <span><FontAwesomeIcon icon={faPlus} className='icon'/>Mostrar Mais</span> : 
+                    <span><FontAwesomeIcon icon={faMinus} className='icon'/>Mostrar Menos</span>}
+                </button>
             </div>
         </section>
     );
