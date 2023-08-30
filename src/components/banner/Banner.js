c55b0887ed67f58a83d7e0cd1c8c2c17b8caeaed
Renan Lima <renan.arantes123@gmail.com>
2023-08-30 11:46:19 -0300

    added video

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Banner.css';
-import renan from '../../img/renan.png';
 import curriculo_eng from '../../files/renan_lima_resume.docx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faScroll } from '@fortawesome/free-solid-svg-icons';
@@ -17,7 +16,7 @@ function Banner() {
                         Download my resume
                     </a>
                 </div>
-                <img src={renan} alt='Foto Renan'/>
+                <iframe width="760" height="410" className='youtube' src="https://www.youtube.com/embed/wYtGfh1omso?si=3W1d3RLrloqJ7nyN" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
         </section>
     );
